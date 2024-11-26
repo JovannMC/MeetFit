ac88453639825e3a781d9e951e469b9d0dcd2749
JovannMC <jovannmc@femboyfurry.net>
2024-11-26 16:43:49 +0300

    Set up skeleton v3 beta

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,20 @@
 import forms from '@tailwindcss/forms';
 import type { Config } from 'tailwindcss';
 
+import { contentPath, skeleton } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
+
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: [forms]
+	plugins: [
+		forms,
+		skeleton({
+			themes: [themes.modern]
+		})
+	]
 } satisfies Config;
