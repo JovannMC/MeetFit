48c89f3f6c4abea402ce125fee5c9055d95a7f4e
JovannMC <jovannmc@femboyfurry.net>
2024-11-30 20:24:10 +0300

    Fill calendar with previous/next days

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -2,28 +2,57 @@
 	import { onMount } from 'svelte';
 	import { info } from '../routes/log';
 
+	let startingDay = 'Monday';
+
 	const date = new Date();
 	const month = date.getMonth();
 	const year = date.getFullYear();
 
-    const daysPrevious = daysInMonth(month - 1, year);
+	const daysPrevious = daysInMonth(month - 1, year);
 	const daysCurrent = daysInMonth(month, year);
-    const daysNext = daysInMonth(month + 1, year);
+	const daysNext = daysInMonth(month + 1, year);
 
-    const daysPreviousObject = Array.from({ length: daysPrevious }, (_, i) => ({
-        day: i + 1,
-        name: new Date(year, month - 1, i + 1).toLocaleDateString('en-US', { weekday: 'long' })
-    }));
+	const daysPreviousObject = Array.from({ length: daysPrevious }, (_, i) => ({
+		day: i + 1,
+		name: new Date(year, month - 1, i + 1).toLocaleDateString('en-US', { weekday: 'long' })
+	}));
 
-    const daysCurrentObject = Array.from({ length: daysCurrent }, (_, i) => ({
-        day: i + 1,
-        name: new Date(year, month, i + 1).toLocaleDateString('en-US', { weekday: 'long' })
-    }));
+	const daysCurrentObject = Array.from({ length: daysCurrent }, (_, i) => ({
+		day: i + 1,
+		name: new Date(year, month, i + 1).toLocaleDateString('en-US', { weekday: 'long' })
+	}));
 
-    const daysNextObject = Array.from({ length: daysNext }, (_, i) => ({
-        day: i + 1,
-        name: new Date(year, month + 1, i + 1).toLocaleDateString('en-US', { weekday: 'long' })
-    }));
+	const daysNextObject = Array.from({ length: daysNext }, (_, i) => ({
+		day: i + 1,
+		name: new Date(year, month + 1, i + 1).toLocaleDateString('en-US', { weekday: 'long' })
+	}));
+
+	const firstDayOfWeek = new Date(year, month, 1).getDay();
+	const lastDayOfWeek = new Date(year, month, daysCurrent).getDay();
+
+	const calendar = {
+		previousMonth:
+			firstDayOfWeek !== 0
+				? daysPreviousObject.slice(-firstDayOfWeek).map((day) => ({
+						...day,
+						month: month - 1,
+						year: month === 0 ? year - 1 : year
+					}))
+				: [],
+		currentMonth: daysCurrentObject.map((day) => ({
+			...day,
+			month: month,
+			year: year
+		})),
+		nextMonth:
+			lastDayOfWeek !== 6
+				? daysNextObject.slice(0, 6 - lastDayOfWeek).map((day) => ({
+						...day,
+						month: month + 1,
+						year: month === 11 ? year + 1 : year
+					}))
+				: []
+	};
 
 	const monthNames = [
 		'January',
@@ -41,19 +70,25 @@
 	];
 
 	function daysInMonth(month: number, year: number) {
-        // 0-indexed months
+		// 0-indexed months
 		return new Date(year, month + 1, 1).getDate();
 	}
 
 	onMount(() => {
 		info(`Date: ${date}`);
-        info(`Days in month of ${monthNames[month - 1]}: ${daysPrevious}`);
+		info(`Days in month of ${monthNames[month - 1]}: ${daysPrevious}`);
 		info(`Days in month of ${monthNames[month]}: ${daysCurrent}`);
-        info(`Days in month of ${monthNames[month + 1]}: ${daysNext}`);
+		info(`Days in month of ${monthNames[month + 1]}: ${daysNext}`);
 
-        info(`Days in month of previous month (${monthNames[month - 1]}): ${JSON.stringify(daysPreviousObject)}`);
-        info(`Days in month of current month (${monthNames[month]}): ${JSON.stringify(daysCurrentObject)}`);
-        info(`Days in month of next month (${monthNames[month + 1]}): ${JSON.stringify(daysNextObject)}`);
+		info(
+			`Days in month of previous month (${monthNames[month - 1]}): ${JSON.stringify(daysPreviousObject)}`
+		);
+		info(
+			`Days in month of current month (${monthNames[month]}): ${JSON.stringify(daysCurrentObject)}`
+		);
+		info(
+			`Days in month of next month (${monthNames[month + 1]}): ${JSON.stringify(daysNextObject)}`
+		);
 	});
 </script>
 
@@ -63,20 +98,47 @@
 		<h1>{monthNames[month]} {year}</h1>
 		<button>&gt;</button>
 	</div>
-	<div class="flex flex-col text-center gap-1">
+	<div class="flex flex-col gap-1 text-center">
 		<div class="flex flex-row gap-1">
-			<div class="bg-secondary-800 w-10">Sun</div>
-			<div class="bg-secondary-800 w-10">Mon</div>
-			<div class="bg-secondary-800 w-10">Tue</div>
-			<div class="bg-secondary-800 w-10">Wed</div>
-			<div class="bg-secondary-800 w-10">Thu</div>
-			<div class="bg-secondary-800 w-10">Fri</div>
-			<div class="bg-secondary-800 w-10">Sat</div>
+			<div class="w-10 bg-secondary-700">Mon</div>
+			<div class="w-10 bg-secondary-700">Tue</div>
+			<div class="w-10 bg-secondary-700">Wed</div>
+			<div class="w-10 bg-secondary-700">Thu</div>
+			<div class="w-10 bg-secondary-700">Fri</div>
+			<div class="w-10 bg-secondary-800">Sat</div>
+			<div class="w-10 bg-secondary-800">Sun</div>
 		</div>
-        <div class="grid grid-cols-7 gap-1 justify-items-center">
-            {#each Array(daysCurrent) as _, i}
-                <div class="bg-tertiary-800 w-7 text-center">{i + 1}</div>
-            {/each}
-        </div>
+		<div class="grid grid-cols-7 gap-1">
+			{#each Array(calendar.previousMonth.length).fill(0) as _, i}
+				<div
+					class="h-10 w-10 content-center {calendar.previousMonth[i].name === 'Saturday' ||
+					calendar.previousMonth[i].name === 'Sunday'
+						? 'bg-secondary-700'
+						: 'bg-secondary-600'}"
+				>
+					{calendar.previousMonth[i].day}
+				</div>
+			{/each}
+			{#each Array(calendar.currentMonth.length).fill(0) as _, i}
+				<div
+					class="h-10 w-10 content-center {calendar.currentMonth[i].name === 'Saturday' ||
+					calendar.currentMonth[i].name === 'Sunday'
+						? 'bg-secondary-700'
+						: 'bg-secondary-600'}"
+				>
+					{calendar.currentMonth[i].day}
+				</div>
+			{/each}
+			{#each Array(calendar.nextMonth.length).fill(0) as _, i}
+				<div
+					class="h-10 w-10 content-center {calendar.nextMonth[i].name === 'Saturday' ||
+					calendar.nextMonth[i].name === 'Sunday'
+						? 'bg-secondary-700'
+						: 'bg-secondary-600'}"
+				>
+					{calendar.nextMonth[i].day}
+				</div>
+			{/each}
+		</div>
 	</div>
 </div>
