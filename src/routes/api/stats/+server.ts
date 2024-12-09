da38a386150364d5f7c1f2e97e4078b78aa4e2ad
JovannMC <jovannmc@femboyfurry.net>
2024-12-09 15:27:19 +0300

    fix stats api

--- src/routes/api/stats/+server.ts
+++ src/routes/api/stats/+server.ts
@@ -1,5 +1,5 @@
 import { db } from '$lib/server/db';
-import { attendee, event, user } from '$lib/server/db/schema';
+import { event as eventTable, user } from '$lib/server/db/schema';
 import { json } from '@sveltejs/kit';
 import { count } from 'drizzle-orm';
 import type { RequestHandler } from './$types';
@@ -7,12 +7,19 @@ import type { RequestHandler } from './$types';
 // Get instance stats
 export const GET: RequestHandler = async () => {
 	const userCount = await db.select({ count: count() }).from(user);
-	const eventCount = await db.select({ count: count() }).from(event);
-	const attendeeCount = await db.select({ count: count() }).from(attendee);
+	const eventCount = await db.select({ count: count() }).from(eventTable);
+
+	const events = await db.select().from(eventTable);
+	let attendeeCount = 0;
+
+	events.forEach((event) => {
+		const attendees = JSON.parse(event.attendees ?? '[]');
+		attendeeCount += attendees.length;
+	});
 
 	return json({
 		users: userCount[0].count,
 		events: eventCount[0].count,
-		attendees: attendeeCount[0].count
+		attendees: attendeeCount
 	});
 };
